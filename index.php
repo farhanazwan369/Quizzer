95b7c8d5a30af24b43c8cd79dc3f60160888235f
farhanazwan369 <farhanazwan369@gmail.com>
2024-06-08 13:13:46 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'templates/header.php'; ?>
+
+<div class="container">
+    <h1>Welcome to Quizzer!</h1>
+    <p>This is the introduction to our web application, Quizzer.</p>
+    <p><a href="login.php" class="login-btn">Log In</a> or <a href="register.php" class="login-btn">Sign Up</a></p>
+</div>
+
+<?php include 'templates/footer.php'; ?>
+
+</body>
+</html>
